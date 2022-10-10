c812091f933d965000fc16533643beeeb1f63b8b
Cheatahh <florian.stachnet@gmail.com>
2022-10-10 17:32:49 +0200

    Labwork02 1/3 + case 'password_keyspace'

--- labwork/main.py
+++ labwork/main.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 """
     Response program for the exercise in T3INF9004: Kryptoanalyse und Methoden-Audit.
 
@@ -6,12 +5,14 @@
     Authors: DHBW Students 200374 & 200357 (2022)
 """
 
+import time
 import json
 import sys
 
 import requests
 
 # import handler functions
+from passwordKeyspaceHandler import password_keyspace_handler
 from caesarCipherHandler import caesar_cipher_handler
 from histogramHandler import histogram_handler
 from strcatHandler import strcat_handler
@@ -20,7 +21,8 @@ from strcatHandler import strcat_handler
 handlers = {
     "strcat": strcat_handler,
     "histogram": histogram_handler,
-    "caesar_cipher": caesar_cipher_handler
+    "caesar_cipher": caesar_cipher_handler,
+    "password_keyspace": password_keyspace_handler
 }
 
 # setup config from system args
@@ -40,63 +42,100 @@ print("Endpoint:", endpoint)
 print("Client ID:", client_id)
 print("Assignment Name:", labwork)
 
+# session setup
+session = requests.Session()
+
 # get assigment
-assignments_response = requests.get(endpoint + "/assignment/" + client_id + "/" + labwork,
-                                    headers=request_headers)
+assignments_response = session.get(endpoint + "/assignment/" + client_id + "/" + labwork,
+                                   headers=request_headers)
 assert assignments_response.status_code == 200
 assignments = assignments_response.json()
 
 print("Assignment Header:", json.dumps(dict(filter(
     lambda pack: pack[0] != "testcases", assignments.items())), indent=2))
 
-# get cases
-testcases = assignments["testcases"]
-
 # setup diagnostics
-total_sample_solutions = 0
-passed_sample_solutions = 0
-total_cases = len(testcases)
-passed_cases = 0
+total_time = {}
+
+total_sample_solutions = {}
+passed_sample_solutions = {}
+
+total_cases = {}
+passed_cases = {}
 
 # process cases
-for testcase in testcases:
+for testcase in assignments["testcases"]:
 
     # get case type
     case_type = testcase["type"]
     print("------ NEW CASE ------")
     print("Case:", json.dumps(testcase, indent=2))
 
+    # diagnostics (cases)
+    if case_type in total_cases:
+        total_cases[case_type] += 1
+    else:
+        total_cases[case_type] = 1
+        passed_cases[case_type] = 0
+        total_time[case_type] = 0
+
     try:
         # lookup & run handler for case type
+        start = time.process_time()
         result = handlers[case_type](testcase["assignment"])
-        print("Result:", result)
+        end = time.process_time()
+        print("Result:", result, "in", end - start, "seconds")
+        total_time[case_type] += end - start
 
         # local test for expected solutions
         if "expect_solution" in testcase:
-            total_sample_solutions += 1
+
+            # diagnostics (testcases)
+            if case_type in total_sample_solutions:
+                total_sample_solutions[case_type] += 1
+            else:
+                total_sample_solutions[case_type] = 1
+                passed_sample_solutions[case_type] = 0
+
             match = result == testcase["expect_solution"]
             print("Matches expectation (local):", match)
+
             if match:
-                passed_sample_solutions += 1
+                passed_sample_solutions[case_type] += 1
 
         # submit result
-        submit_result_response = requests.post(endpoint + "/submission/" + testcase["tcid"],
-                                               headers=request_headers, json=result)
+        submit_result_response = session.post(endpoint + "/submission/" + testcase["tcid"],
+                                              headers=request_headers, json=result)
         assert submit_result_response.status_code == 200
         submit_result = submit_result_response.json()
         print("Submit Result:", submit_result)
 
         if submit_result["status"] == "pass":
-            passed_cases += 1
+            passed_cases[case_type] += 1
 
     except KeyError:
         print("Error: Could not match type", case_type)
 
+session.close()
+
 # print diagnostics conclusion
 print("------ CONCLUSION ------")
-print("Total sample solutions:", total_sample_solutions)
-print("Passed sample solutions:", passed_sample_solutions,
-      "[{:.0%}]".format(float(passed_sample_solutions) / float(total_sample_solutions)))
-print("Total cases:", total_cases)
-print("Passed cases:", passed_cases,
-      "[{:.0%}]".format(float(passed_cases) / float(total_cases)))
+
+print("Total sample solutions:", sum(total_sample_solutions.values()))
+print("Passed sample solutions:")
+for case_type, passes in passed_sample_solutions.items():
+    # pretty print
+    print("\t'{case_type}' {passes} of {total} [{percent:.0%}]".format(
+        case_type=case_type, passes=passes, total=total_sample_solutions[case_type],
+        percent=float(passes) / float(total_sample_solutions[case_type]))
+    )
+
+print("Total cases:", sum(total_cases.values()))
+print("Passed cases:")
+for case_type, passes in passed_cases.items():
+    # pretty print
+    print("\t'{case_type}' {passes} of {total} [{percent:.0%}] in {seconds} seconds".format(
+        case_type=case_type, passes=passes, total=total_cases[case_type],
+        percent=float(passes) / float(total_cases[case_type]),
+        seconds=total_time[case_type]
+    ))
---
+++ labwork/passwordKeyspaceHandler.py
@@ -0,0 +1,128 @@
+"""
+    This file is a handler module for response program (T3INF9004: Kryptoanalyse und Methoden-Audit).
+
+    License: CC-0
+    Authors: DHBW Students 200374 & 200357 (2022)
+
+    Functions:
+
+    password_keyspace_handler
+"""
+from functools import reduce
+from itertools import product
+from string import ascii_lowercase, ascii_uppercase, digits
+
+
+# This function contains multiple approaches to solve this problem, see comments for detail
+def password_keyspace_handler(assignment):
+    """Handler-function for the 'password_keyspace' type."""
+
+    # shortcut and safety
+    length = assignment["length"]
+    if length <= 0:
+        return 0
+
+    # extract special chars from alphabet, do not have to rely on presets
+    special_chars = "".join({*assignment["alphabet"]} - {*(ascii_lowercase + ascii_uppercase + digits)})
+
+    # generate all possible strings of length 'length' by
+    possibilities = ["".join(item) for item in product(assignment["alphabet"], repeat=length)]
+
+    # test if a possible value is failing a given restriction
+    # hashtable lookup => faster, see another version down below
+    """
+    is_failing_restriction = {
+        "at_least_one_special_char":
+            # not any = none
+            # if no char is a special char -> fail
+            lambda value: not any(char in special_chars for char in value),
+        "at_least_one_uppercase_char":
+            # if no char is uppercase -> fail
+            lambda value: not any(char.isupper() for char in value),
+        "at_least_one_lowercase_char":
+            # if no char is lowercase -> fail
+            lambda value: not any(char.islower() for char in value),
+        "at_least_one_digit":
+            # if no char is a digit -> fail
+            lambda value: not any(char.isdigit() for char in value),
+        "no_consecutive_same_char":
+            # iterate over each index (except last char, 0 <= index < length - 1)
+            # compare current char and next char
+            # if there are any consecutive chars -> fail
+            lambda value: any(char == value[index + 1] for index, char in enumerate(value[:-1])),
+        "special_char_not_last_place":
+            # if last char is special char -> fail
+            lambda value: value[-1] in special_chars
+    }
+    """
+
+    # slower, but more robust version of 'is_failing_restriction'
+    """
+    def is_failing_restriction(restriction, value):
+        match restriction:
+            case "at_least_one_special_char":
+                return not any(char in special_chars for char in value)
+            case "at_least_one_uppercase_char":
+                return not any(char.isupper() for char in value)
+            case "at_least_one_lowercase_char":
+                return not any(char.islower() for char in value)
+            case "at_least_one_digit":
+                return not any(char.isdigit() for char in value)
+            case "no_consecutive_same_char":
+                return any(char == value[index + 1] for index, char in enumerate(value[:-1]))
+            case "special_char_not_last_place":
+                return value[-1] in special_chars 
+    """
+
+    # initial approach, count all possible strings passing all restrictions
+    """
+    count = 0
+    for possibility in possibilities:
+        # not any = none; if no restrictions fail -> pass
+        if not any(is_failing_restriction[restriction](possibility) for restriction in assignment["restrictions"]):
+            count += 1
+            
+    return {
+        "count": count
+    }
+    """
+
+    def filter_by_restriction(values, res):
+        match res:
+            case "at_least_one_special_char":
+                # if any char is a special char -> pass filter
+                return filter(lambda value: any(char in special_chars for char in value), values)
+            case "at_least_one_uppercase_char":
+                # if any char is uppercase -> pass filter
+                return filter(lambda value: any(char.isupper() for char in value), values)
+            case "at_least_one_lowercase_char":
+                # if any char is lowercase -> pass filter
+                return filter(lambda value: any(char.islower() for char in value), values)
+            case "at_least_one_digit":
+                # if any char a digit -> pass filter
+                return filter(lambda value: any(char.isdigit() for char in value), values)
+            case "no_consecutive_same_char":
+                # iterate over each index (except last char, 0 <= index < length - 1)
+                # compare current char and next char
+                # if there are [not any] = [none] consecutive chars -> pass filter
+                return filter(lambda value: not any(char == value[index + 1] for index, char in enumerate(value[:-1])),
+                              values)
+            case "special_char_not_last_place":
+                # if last char is not a special char -> pass filter
+                return filter(lambda value: value[-1] not in special_chars, values)
+
+    # second approach, filter possibilities with restrictions
+    # seems to be ~30% faster, memory usage stays the same
+    """
+    for restriction in assignment["restrictions"]:
+        possibilities = filter_by_restriction(restriction, possibilities)
+    """
+
+    # using second approach
+    # using reduce seems to be even faster (~7%)!?
+    # noinspection PyTypeChecker
+    possibilities = reduce(filter_by_restriction, assignment["restrictions"], possibilities)
+
+    return {
+        "count": len([*possibilities])
+    }
